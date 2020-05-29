72c0dede21276d5a4497628fee4e0916b0ca5e76
Jagraj Singh <singhjagraj900@gmail.com>
2020-05-30 00:17:38 +0100

    v2
    Updated login page

--- octopus_app/lib/main.dart
+++ octopus_app/lib/main.dart
@@ -17,8 +17,8 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      resizeToAvoidBottomPadding: true,
+      backgroundColor: Colors.deepPurple[900],
+      resizeToAvoidBottomInset: false,
 
       // code for entire body
       body: Stack(
@@ -52,136 +52,138 @@ class _HomeState extends State<Home> {
           ),
         ),
 
-        // login in centre of page
-        Container(
-          child: Center(
-            child: Text(
-              'Login:',
-              style: TextStyle(
-                fontSize: 40.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.lightBlueAccent,
-                fontFamily: 'Gotham',
-              ),
-            ),
-          ),
-        ),
-        
-        // email address
-        Container(
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 120.0),
-              child: Text(
-                'Email Address:',
-                style: TextStyle(
-                  fontSize: 30.0,
-                  color: Colors.amberAccent,
-                  fontFamily: 'Gotham',
-                  ),
-                ),
-              ),
-            ),
-          ),
-        
-        // space to enter email
-        Container(
-          height: 60.0,
-          margin: EdgeInsets.only(top: 600.0, left: 40.0, right: 40.0, bottom: 380.0),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(50),
-            color: Colors.grey[300],
-          ),
-          child: Center(
-            child: Text(
-              '',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 23.0,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Gotham',
-              ),
-            ),
-          ),
-        ),
-
-        // password
-        Container(
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 360.0),
-              child: Text(
-                'Password:',
-                style: TextStyle(
-                  fontSize: 30.0,
-                  color: Colors.amberAccent,
-                  fontFamily: 'Gotham',
-                  ),
-                ),
-              ),
-            ),
-          ),
-
-        /// space to enter password
-        Container(
-          height: 60.0,
-          margin: EdgeInsets.only(top: 720.0, left: 40.0, right: 40.0, bottom: 260.0),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(50),
-            color: Colors.grey[300],
-          ),
-          child: Center(
-            child: Text(
-              '',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 23.0,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Gotham',
-              ),
-            ),
-          ),
-        ),
-
-        // forgot password
-        Container(
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 600.0),
-              child: Text(
-                'Forgot Password?',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.redAccent,
-                  fontFamily: 'Gotham',
-                  ),
-                ),
-              ),
-            ),
-          ),
-
-        // login button
-        Container(
-          child: Padding(
-            padding: const EdgeInsets.only(top: 720.0),
+          // login in centre of page
+          Container(
             child: Center(
-              child: FlatButton(
-                onPressed: () {},
-                color: Colors.greenAccent,
+              child: Text(
+                'Login',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.lightBlueAccent,
+                  fontFamily: 'Gotham',
+                ),
+              ),
+            ),
+          ),
+        
+          // email address
+          Container(
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 120.0),
                 child: Text(
-                  'Login',
+                  'Email Address:',
                   style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 25.0,
-                    fontWeight: FontWeight.bold,
+                    fontSize: 30.0,
+                    color: Colors.amberAccent,
                     fontFamily: 'Gotham',
+                    ),
+                  ),
+                ),
+              ),
+            ),
+        
+          // space to enter email
+          Container(
+            margin: EdgeInsets.only(top: 620.0, left: 50.0, right: 50.0, bottom: 370.0),
+            decoration: BoxDecoration(
+              border: Border(bottom: BorderSide(color: Colors.grey)),
+            ),
+            child: Center(
+              child: TextField(
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25.0,
+                  fontFamily: 'Gotham',
+                ),
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  hintStyle: TextStyle(color: Colors.white),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ),
+
+          // password
+          Container(
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 380.0),
+                child: Text(
+                  'Password:',
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    color: Colors.amberAccent,
+                    fontFamily: 'Gotham',
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+          /// space to enter password
+          Container(
+            margin: EdgeInsets.only(top: 740.0, left: 50.0, right: 50.0, bottom: 240.0),
+            decoration: BoxDecoration(
+              border: Border(bottom: BorderSide(color: Colors.grey)),
+            ),
+            child: Center(
+              child: TextField(
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25.0,
+                  fontFamily: 'Gotham',
+                ),
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  hintStyle: TextStyle(color: Colors.white),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ),
+
+          // forgot password
+          Container(
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 640.0),
+                child: Text(
+                  'Forgot Password?',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.redAccent,
+                    fontFamily: 'Gotham',
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+          // login button
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 750.0),
+              child: Center(
+                child: RaisedButton(
+                  onPressed: () {},
+                  color: Colors.greenAccent,
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Gotham',
+                    ),
                   ),
                 ),
               ),
             ),
           ),
-        ),
 
 
         ],
