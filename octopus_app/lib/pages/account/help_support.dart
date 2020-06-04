131ab1dc2a8276b1966811acb3a69bd2ceaa31f8
Jagraj Singh <singhjagraj900@gmail.com>
2020-06-04 01:35:54 +0100

    v2.9
    Added content to help_support page

--- octopus_app/lib/pages/account/help_support.dart
+++ octopus_app/lib/pages/account/help_support.dart
@@ -36,6 +36,179 @@ class _HelpSupportState extends State<HelpSupport> {
                   ),
                 ),
               ),
+
+              Container(
+                margin: const EdgeInsets.only(top: 40.0),
+                child: Text(
+                  'Contact Us:',
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    fontFamily: 'Gotham',
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+
+              Container(
+                margin: const EdgeInsets.only(top: 10.0),
+                child: Text(
+                  'Online is the best way to get in\ntouch with our customer service\nteam, 7 days a week, and we\nusually reply to enquiries within\na few hours!',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontFamily: 'Gotham',
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+
+              Container(
+                margin: const EdgeInsets.only(top: 10.0),
+                padding: const EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 2.0,
+                    color: Colors.pinkAccent[400],
+                  ),
+                  borderRadius: BorderRadius.circular(50.0),
+                ),
+                child: Text(
+                  'hello@octopus.energy',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontFamily: 'Gotham',
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+
+              Container(
+                margin: const EdgeInsets.only(top: 40.0),
+                child: Text(
+                  'Business Enquiries:',
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    fontFamily: 'Gotham',
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+
+              Container(
+                margin: const EdgeInsets.only(top: 10.0),
+                padding: const EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 2.0,
+                    color: Colors.pinkAccent[400],
+                  ),
+                  borderRadius: BorderRadius.circular(50.0),
+                ),
+                child: Text(
+                  'business@octopus.energy',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontFamily: 'Gotham',
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+
+              Container(
+                margin: const EdgeInsets.only(top: 40.0),
+                padding: const EdgeInsets.all(20.0),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 3.0,
+                    color: Colors.amberAccent[400],
+                  ),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Text(
+                  'You can also message us\n on Facebook or Twitter!',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontFamily: 'Gotham',
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+
+              Container(
+                margin: const EdgeInsets.only(top: 40.0),
+                child: Text(
+                  'Speak to us:',
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    fontFamily: 'Gotham',
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+
+              Container(
+                margin: const EdgeInsets.only(top: 10.0),
+                child: Text(
+                  'If you have an urgent issue you\n    can always call us for help.',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontFamily: 'Gotham',
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+
+              Container(
+                margin: const EdgeInsets.only(top: 10.0),
+                padding: const EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 2.0,
+                    color: Colors.pinkAccent[400],
+                  ),
+                  borderRadius: BorderRadius.circular(50.0),
+                ),
+                child: Text(
+                  '0808 164 1088',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontFamily: 'Gotham',
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+
+              Container(
+                margin: const EdgeInsets.only(top: 10.0),
+                child: Text(
+                  'For business customers:',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontFamily: 'Gotham',
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+
+              Container(
+                margin: const EdgeInsets.only(top: 10.0),
+                padding: const EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 2.0,
+                    color: Colors.pinkAccent[400],
+                  ),
+                  borderRadius: BorderRadius.circular(50.0),
+                ),
+                child: Text(
+                  '020 3389 5613',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontFamily: 'Gotham',
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+
             ],
           ),
         ),
